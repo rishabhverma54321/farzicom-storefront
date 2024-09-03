bc8e08df13a0c73d9609b57bf55e444c85ce4bce
rishabhverma54321 <rishabhverma54321@gmail.com>
2024-09-03 13:54:11 +0530

    fix: sampleRate and serviceWorkerTimout issue fix

--- src/constants.ts
+++ src/constants.ts
@@ -3,10 +3,10 @@ export const restApiUrl = process.env.NEXT_PUBLIC_REST_API_URL || process.env.RE
 export const apiUrlSSR =
   process.env.NEXT_PUBLIC_API_INTERNAL_HOST || process.env.NEXT_PUBLIC_API_URI;
 export const sentryDsn = process.env.SENTRY_DSN;
-const sampleRate = parseFloat(process.env.SENTRY_APM);
-export const sentrySampleRate = isNaN(sampleRate) ? 0 : sampleRate;
-export const serviceWorkerTimeout =
-  parseInt(process.env.SERVICE_WORKER_TIMEOUT, 10) || 60 * 1000;
+// const sampleRate = parseFloat(process.env.SENTRY_APM);
+// export const sentrySampleRate = isNaN(sampleRate) ? 0 : sampleRate;
+// export const serviceWorkerTimeout =
+//   parseInt(process.env.SERVICE_WORKER_TIMEOUT, 10) || 60 * 1000;
 export const demoMode = process.env.DEMO_MODE === "true";
 export const gtmId = process.env.GTM_ID;
 export const clevertapID = process.env.CLEVERTAP_ID;
